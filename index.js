8034630c343c25d51ed992ea56b04126f5fa6db1
debkred <nielslassbo@hotmail.com>
2025-07-14 12:31:51 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px' }}>
-      <h1>Välkommen till ChatPT</h1>
-      <p>Din AI-drivna personliga tränare online</p>
-    </div>
-  );
-}
