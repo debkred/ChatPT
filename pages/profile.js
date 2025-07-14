d8c6a220d91dd2d360214da6677ae02449b2969e
debkred <nielslassbo@hotmail.com>
2025-07-14 19:18:57 +0200

    Delete pages directory

--- pages/profile.js
+++
@@ -1,8 +0,0 @@
-export default function Profile() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>Fyll i din träningsprofil</h2>
-      <p>Formulär kommer här...</p>
-    </div>
-  );
-}
