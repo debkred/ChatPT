9cf137c3374e7f05e1886ce2d3e463e723469684
debkred <nielslassbo@hotmail.com>
2025-07-14 12:58:53 +0200

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'Arial', padding: 20 }}>
-      <h1>Välkommen till ChatPT</h1>
-      <p>Din AI-drivna personliga tränare online.</p>
-    </div>
-  );
-}
