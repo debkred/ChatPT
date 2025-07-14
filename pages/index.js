d673c45f804fe019948ea1e1551969cd1dfd0d26
debkred <nielslassbo@hotmail.com>
2025-07-14 12:18:24 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial', padding: 20 }}>
+      <h1>Välkommen till ChatPT</h1>
+      <p>Din AI-drivna personliga tränare online.</p>
+    </div>
+  );
+}
