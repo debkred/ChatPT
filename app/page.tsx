ba3f1f8c2839cd7fba86cecf4f2739672204be73
debkred <nielslassbo@hotmail.com>
2025-07-16 15:50:39 +0200

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main>
-      <h1>Välkommen till ChatPT</h1>
-      <a href="/profil">Starta din träningsprofil</a>
-    </main>
-  );
-}
--- app/resultat/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Resultat() {
-  return (
-    <main>
-      <h2>Ditt träningsupplägg</h2>
-      <p>Här visas ett AI-genererat program baserat på din profil.</p>
-    </main>
-  );
-}
