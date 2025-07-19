4f2b6e8f1e70d426ce7c6f47f9fdbd26c077f09b
debkred <nielslassbo@hotmail.com>
2025-07-19 22:15:37 +0200

    Delete lib directory

--- lib/generateWorkoutPlan.ts
+++
@@ -1,3 +0,0 @@
-export function generateWorkoutPlan(data: Record<string, string>) {
-  return `Mål: ${data.goal}. Erfarenhet: ${data.experience}. Träningsfrekvens: ${data.frequency}. Tidsram: ${data.timeframe}. Plan kommer genereras baserat på detta.`;
-}
